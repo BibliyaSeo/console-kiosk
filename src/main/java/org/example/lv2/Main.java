82d8797566a8e256b15e7a69d0441175c9aa1187
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-07-16 18:04:53 +0900

    docs: 햄버거 설명 문구 수정

--- src/main/java/org/example/lv2/Main.java
+++ src/main/java/org/example/lv2/Main.java
@@ -11,9 +11,9 @@ public class Main {
 
         // 메뉴 설정
         menuItems.add(new MenuItem(1, "ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem(2, "SmokeShack", 8.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem(3, "Cheeseburger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem(4, "Hamburger", 5.4, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem(2, "SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem(3, "Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem(4, "Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
         // 메뉴 출력
         System.out.println("[ SHAKESHACK MENU ]");
