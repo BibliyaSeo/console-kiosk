6aeffa540fd7b03190e5ef80326276c375240c96
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-07-21 17:05:33 +0900

    feat: 장바구니 추가 기능

---
+++ src/main/java/org/example/lv6/Kiosk.java
@@ -0,0 +1,131 @@
+package org.example.lv6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    // 스캐너 선언
+    Scanner scanner = new Scanner(System.in);
+    // 클래스에 바로 . 하지 말라고 하셨는데 더미데이터는 거의 static으로 사용하길래...
+    private final List<Menu> menus = List.of(
+            MenuData.getBurger(),
+            MenuData.getDrinks(),
+            MenuData.getDesserts()
+    );
+
+    List<MenuItem> shoppingCart = new ArrayList<>();
+
+    private boolean showMainMenu = true;
+    private Menu menuByCategory = null;
+
+    public void start() {
+        // 반복문 시작
+        while (true) {
+            if (showMainMenu) {
+                // List와 Menu 클래스 활용하여 상위 카테고리 메뉴 출력
+                System.out.println("[MAIN MENU]");
+                for (int i = 0; i < menus.size(); i++) {
+                    System.out.println((i + 1) + ". " + menus.get(i).getCategory());
+                }
+                System.out.println("0. 종료      | 종료");
+
+                // 숫자 입력 받기
+                int choiceMain = getChoiceMenu("메인");
+
+                // 프로그램 종료
+                if (choiceMain == 0) {
+                    System.out.println("키오스크 프로그램을 종료합니다.");
+                    scanner.close();
+                    return;
+                }
+
+                // 에러 처리
+                if (choiceMain == -9999) {
+                    System.out.println("숫자를 입력해 주세요.");
+                    System.out.println();
+                    continue;
+                }
+
+                if (choiceMain < 0 || choiceMain > menus.size()) {
+                    System.out.println("해당 번호의 메뉴가 없습니다.");
+                    System.out.println();
+                } else {
+                    // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+                    menuByCategory = menus.get(choiceMain - 1);
+                    // 메인메뉴 안 보여 주고 서브 메뉴로 넘어가기
+                    showMainMenu = false;
+                }
+            } else {
+                // 메인 메뉴 말고 카테고리별 메뉴 보여주기
+                System.out.println();
+                System.out.println("[" + menuByCategory.getCategory().toUpperCase() + " MENU]");
+                List<MenuItem> menuItems = menuByCategory.getMenuItems();
+                // Menu가 가진 List<MenuItem>을 반복문을 활용하여 햄버거 메뉴 출력
+                for (int i = 0; i < menuItems.size(); i++) {
+                    System.out.println((i + 1) + ". " + menuItems.get(i));
+                }
+                System.out.println("0. 메인 메뉴로    | 뒤로가기");
+
+                // 숫자 입력 받기
+                int choiceMenu = getChoiceMenu(menuByCategory.getCategory());
+
+                // 뒤로 가기
+                if (choiceMenu == 0) {
+                    System.out.println();
+                    showMainMenu = true;
+                    // continue 안 넣어주면 안 됨!
+                    continue;
+                }
+
+                // 에러 처리
+                if (choiceMenu == -9999) {
+                    System.out.println("숫자를 입력해 주세요.");
+                    System.out.println();
+                    continue;
+                }
+
+                if (choiceMenu < 0 || choiceMenu > menuByCategory.getMenuItems().size()) {
+                    System.out.println("해당 번호의 메뉴가 없습니다.");
+                    System.out.println();
+                } else {
+                    // 입력 받은 숫자가 올바르다면 인덱스로 활용해서 Menu가 가지고 있는 List<MenuItem>에 접근하기
+                    // menu.getMenuItems().get(i); 같은 형식으로 하나씩 들어가서 얻어와야 합니다.
+                    MenuItem menuItemByCategory = menuByCategory.getMenuItems().get(choiceMenu - 1);
+                    System.out.println();
+                    System.out.println(menuItemByCategory);
+
+                    // 장바구니 추가
+                    System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+                    System.out.println("1. 확인         2. 취소");
+
+                    if (scanner.hasNextInt()) {
+                        int choice = scanner.nextInt();
+                        if (choice == 1) {
+                            shoppingCart.add(menuItemByCategory);
+                            System.out.println();
+                            System.out.println(menuItemByCategory.getName() + " 이 장바구니에 추가되었습니다.");
+                            System.out.println();
+                            showMainMenu = true;
+                        }
+                    } else {
+                        System.out.println("1번과 2번 중 골라주세요.");
+                        scanner.nextLine();
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    private int getChoiceMenu(String label) {
+        System.out.print(label + " 메뉴의 번호를 선택해 주세요: ");
+        if (scanner.hasNextInt()) {
+            return scanner.nextInt();
+        } else {
+            scanner.nextLine();
+            return -9999;
+        }
+    }
+}
---
+++ src/main/java/org/example/lv6/MenuItem.java
@@ -0,0 +1,24 @@
+package org.example.lv6;
+
+public class MenuItem {
+    // 이름, 가격, 설명 필드 선언하여 관리
+    private final String name;
+    private final double price;
+    private final String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    // 구조에 맞게 함수를 선언해놓고 가져다 사용하세요.
+    @Override
+    public String toString() {
+        return name + " | W " + price + " | " + description;
+    }
+}
