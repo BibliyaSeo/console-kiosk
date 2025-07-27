449cdc3a229c55b3063356d983b0fde53dcb03c6
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-07-27 09:50:57 +0900

    feat: answer

---
+++ src/main/java/org/example/answer/Menu.java
@@ -0,0 +1,22 @@
+package org.example.answer;
+
+import java.util.List;
+
+public class Menu {
+
+    private final String name;
+    private final List<MenuItem> menuItems;
+
+    public Menu(String name, List<MenuItem> menuItems) {
+        this.name = name;
+        this.menuItems = menuItems;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+}
